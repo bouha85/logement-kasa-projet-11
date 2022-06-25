f507925f369ba40ea92c2a6d51e1deec1611a692
bouha85 <bouha_k@hotmail.fr>
2022-06-25 11:26:47 +0200

    ajouter le fichier json

--- src/components/Error.js
+++ src/components/Error.js
@@ -1,8 +1,9 @@
 import React from 'react'
-
+import '../styles/Error.css'
+import '../styles/index.css'
 const Error = () => {
   return (
-    <div>
+    <div className="error">
       <div className="error__title">404</div>
       <h1 className="error__subtitle">
         Oups! La page que vous demandez n'existe pas
